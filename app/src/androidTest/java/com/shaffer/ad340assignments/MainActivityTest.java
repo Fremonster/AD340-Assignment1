8f7908da5f3dcfbba44adbb13d3651c4e21c3bb7
Melanie Shaffer <melanie.shaffer@gmail.com>
2018-04-25 15:20:45 -0700

    Another Assignment 3 Commit

--- app/src/androidTest/java/com/shaffer/ad340assignments/MainActivityTest.java
+++ app/src/androidTest/java/com/shaffer/ad340assignments/MainActivityTest.java
@@ -34,7 +34,6 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> testRule = new ActivityTestRule<>(MainActivity.class);
 
-    // works in landscape orientation, also
     @Test
     public void hello_goodbyeWithRotate() {
         onView(withId(R.id.textView)).check(matches(withText(R.string.my_name)));
@@ -79,7 +78,6 @@ public class MainActivityTest {
     }
 
     // check that get expected errors when enter no text into fields
-    // works in landscape orientation, also
     @Test
     public void getExpectedEditTextErrors() {
         closeSoftKeyboard();
@@ -111,7 +109,6 @@ public class MainActivityTest {
     }
 
     // requires minimum of 5 characters
-    // works in landscape orientation, also
     @Test
     public void invalidUsername() {
         onView(withId(R.id.usernameEditText)).perform(typeText("mel"), closeSoftKeyboard());
@@ -127,6 +124,18 @@ public class MainActivityTest {
         onView(withId(R.id.emailEditText)).check(matches(hasErrorText("Please enter a valid email address")));
     }
 
+    @Test
+    public void testUnderEighteen() {
+        onView(withId(R.id.nameEditText)).perform(typeText("Peter Parker"), closeSoftKeyboard());
+        onView(withId(R.id.emailEditText)).perform(typeText("spiderman@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.usernameEditText)).perform(typeText("spidey"), closeSoftKeyboard());
+        onView(withId(R.id.occupationEditText)).perform(typeText("Superhero"), closeSoftKeyboard());
+        onView(withId(R.id.descriptionEditText)).perform(typeText("Looking for someone who will be understanding when I flake out on a date to fight crime. Must love science."), closeSoftKeyboard());
+        onView(withId(R.id.in_date)).perform(typeText("05/10/2000"), closeSoftKeyboard());
+        onView(withId(R.id.submit_btn)).perform(scrollTo(), click());
+        onView(withId(R.id.in_date)).check(matches(hasErrorText("Must be 18 or older to register.")));
+    }
+
     // date format must be MM/DD/YYYY
     @Test
     public void invalidDateFormat() {
@@ -135,23 +144,42 @@ public class MainActivityTest {
         onView(withId(R.id.in_date)).check(matches(hasErrorText("Please enter a valid date in the MM/DD/YYYY format")));
     }
 
+    @Test
+    public void ageTextViewWithRotate() {
+        onView(withId(R.id.nameEditText)).perform(typeText("Benedict Cumberbatch"), closeSoftKeyboard());
+        onView(withId(R.id.emailEditText)).perform(typeText("benedict@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.usernameEditText)).perform(typeText("bcumberbatch"), closeSoftKeyboard());
+        onView(withId(R.id.occupationEditText)).perform(typeText("Actor"), closeSoftKeyboard());
+        onView(withId(R.id.descriptionEditText)).perform(typeText("I like long walks on the beach and holding hands. I am looking for someone who loves bunnies and chocolate. I enjoy underwater basket weaving."), closeSoftKeyboard());
+        onView(withId(R.id.in_date)).perform(typeText("07/19/1976"), closeSoftKeyboard());
+        onView(withId(R.id.ageTextView)).check(matches(withText(R.string.your_age_is)));
+        onView(withId(R.id.confirm_age)).perform(scrollTo(), click());
+        onView(withId(R.id.ageTextView)).check(matches(withText("Your age is 41. Please click below to register.")));
+        TestUtils.rotateScreen(testRule.getActivity());
+        closeSoftKeyboard();
+        onView(withId(R.id.ageTextView)).check(matches(withText("Your age is 41. Please click below to register.")));
+    }
+
     // tests datepicker
-    // works in landscape orientation
     @Test
     public void testDatePicker() {
         TestUtils.setDate(R.id.date_btn, 1976, 7, 19);
         onView(withId(R.id.in_date)).check(matches(withText("07/19/1976")));
     }
 
+
     @Test
-    public void ageTextViewWithRotate() {
-        onView(withId(R.id.in_date)).perform(typeText("10/08/1974"), closeSoftKeyboard());
+    public void testTextViewUnderEighteen() {
+        onView(withId(R.id.nameEditText)).perform(typeText("Peter Parker"), closeSoftKeyboard());
+        onView(withId(R.id.emailEditText)).perform(typeText("spiderman@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.usernameEditText)).perform(typeText("spidey"), closeSoftKeyboard());
+        onView(withId(R.id.occupationEditText)).perform(typeText("Superhero"), closeSoftKeyboard());
+        onView(withId(R.id.descriptionEditText)).perform(typeText("Looking for someone who will be understanding when I flake out on a date to fight crime. Must love science."), closeSoftKeyboard());
+        onView(withId(R.id.in_date)).perform(typeText("05/10/2000"), closeSoftKeyboard());
         onView(withId(R.id.ageTextView)).check(matches(withText(R.string.your_age_is)));
         onView(withId(R.id.confirm_age)).perform(scrollTo(), click());
-        onView(withId(R.id.ageTextView)).check(matches(withText("Your age is 43. Please click below to register.")));
-        TestUtils.rotateScreen(testRule.getActivity());
+        onView(withId(R.id.ageTextView)).check(matches(withText("Your age is 17. You must be at least 18 to register.")));
     }
-
 
 
 
